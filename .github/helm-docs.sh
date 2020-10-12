f853ff21ad7f6d69c194aa32c891e0eef7e53732
Endre Czirbesz <endre.czirbesz@rungway.com>
2020-10-12 14:39:32 +0100

    Fixing script

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 HELM_DOCS_VERSION="1.3.0"
-PWD="$(pwd)"
+WORK_DIR="$(pwd)"
 
 # install helm-docs
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
@@ -12,7 +12,7 @@ tar -xf /tmp/helm-docs.tar.gz helm-docs
 # validate docs
 for CHART_DIR in ${CHART_DIRS}; do
   pushd $CHART_DIR
-  $PWD/helm-docs
+  ${WORK_DIR}/helm-docs
   popd
 done
 git diff --exit-code
